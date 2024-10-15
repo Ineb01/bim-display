88cf305c2c4d8dc8ef5dfe13d9dc174d0a556025
Benjamin Bachmayr <benjamin.bachmayr@gmail.com>
2024-10-15 12:08:27 +0000

    dummy stuff

--- python/dummy.py
+++ python/dummy.py
@@ -1,2 +1,7 @@
 def dummy():
-    return "dumb"
+    return [
+        {"line": 1, "time": 4, "dir": "Univ.", "stop": "Moz."},
+        {"line": 2, "time": 5, "dir": "Univ.", "stop": "Moz."},
+        {"line": 3, "time": 8, "dir": "Hbf.", "stop": "Burg."},
+        {"line": 4, "time": 9, "dir": "Hbf.", "stop": "Burg."}
+    ]
