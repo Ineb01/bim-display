ba6a2632336d427b49984909dcf22c51a7848a18
Benjamin Bachmayr <benjamin.bachmayr@gmail.com>
2024-10-21 22:38:51 +0200

    added some changing data

--- python/dummy.py
+++ python/dummy.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 def dummy():
     return [
-        {"line": 1, "time": 4, "dir": "Univ.", "stop": "Moz."},
-        {"line": 2, "time": 5, "dir": "Univ.", "stop": "Moz."},
-        {"line": 3, "time": 8, "dir": "Hbf.", "stop": "Burg."},
-        {"line": 4, "time": 9, "dir": "Hbf.", "stop": "Burg."}
+        {"line": 1, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Univ.", "stop": "Moz."},
+        {"line": 2, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Univ.", "stop": "Moz."},
+        {"line": 3, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Hbf.", "stop": "Burg."},
+        {"line": 4, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Hbf.", "stop": "Burg."},
     ]
