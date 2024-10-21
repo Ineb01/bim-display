048dd284a8b7337eaff9a97ad95d9dd4c2327ab7
Gatsch <36137844+Gatsch@users.noreply.github.com>
2024-10-21 23:21:26 +0200

    Update API specification

--- python/dummy.py
+++ python/dummy.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 def dummy():
     return [
-        {"line": 1, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Univ.", "stop": "Moz."},
-        {"line": 2, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Univ.", "stop": "Moz."},
-        {"line": 3, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Hbf.", "stop": "Burg."},
-        {"line": 4, "time": datetime.now().strftime("%H:%M:%S"), "dir": "Hbf.", "stop": "Burg."},
+        {"line": "1", "eta": datetime.now().strftime("%H:%M:%S"), "etl": datetime.now().strftime("%H:%M:%S"), "time": 1, "dir": "Univ.", "stop": "Moz."},
+        {"line": "2", "eta": datetime.now().strftime("%H:%M:%S"), "etl": datetime.now().strftime("%H:%M:%S"), "time": 15, "dir": "Univ.", "stop": "Moz."},
+        {"line": "3", "eta": datetime.now().strftime("%H:%M:%S"), "etl": datetime.now().strftime("%H:%M:%S"), "time": 35, "dir": "Hbf.", "stop": "Burg."},
+        {"line": "4", "eta": datetime.now().strftime("%H:%M:%S"), "etl": datetime.now().strftime("%H:%M:%S"), "time": int(datetime.now().strftime("%S")), "dir": "Hbf.", "stop": "Burg."},
     ]
